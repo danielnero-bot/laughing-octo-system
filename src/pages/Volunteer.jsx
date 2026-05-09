cb6710b327d1087d811b2b41733fc40bd3456f48
Somkenechukwu Mamah <somkenemamah@gmail.com>
2026-05-09 13:27:24 +0100

    Update Volunteer.jsx
    Remove Preferred not to say in Gender

--- src/pages/Volunteer.jsx
+++ src/pages/Volunteer.jsx
@@ -246,7 +246,6 @@ const Volunteer = () => {
                   <option value="">Select Gender</option>
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
-                  <option value="Other">Other / Prefer not to say</option>
                 </select>
 
                 <input
